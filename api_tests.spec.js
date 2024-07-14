5886fc3dafbd9d51d117c361832cde823f05cd0d
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-14 13:41:57 +0530

    Test updated

--- api_tests.spec.js
+++ api_tests.spec.js
@@ -1,7 +1,7 @@
 ///<reference types ="cypress"/>
 describe('JSON place holder API testing', () => {
     const baseUrl = "https://jsonplaceholder.typicode.com" ;
-    
+    //using get method to fetch response
     it('Fetch a list of posts', () => {
         cy.request({
             method: 'GET',
@@ -15,6 +15,7 @@ describe('JSON place holder API testing', () => {
         }) 
         
     });
+     //using post method to create new entry
     it('Create a new post', () => {
         cy.request({
             method: 'POST',
@@ -29,7 +30,37 @@ describe('JSON place holder API testing', () => {
             }
         }).then((Response)=>{
             expect(Response.status).to.equal(201);
-          
+          }) 
+    });      
+     //using update method to update resource
+    it('Update an existing post', () => {
+        cy.request({
+            method: 'PUT',
+            url: baseUrl + "/posts/1", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                 "title": "Updated Post", 
+                 "body": "This is the updated body of the post",
+                  "userId": 1
+            }
+        }).then((Response)=>{
+            expect(Response.status).to.equal(200); 
         }) 
-});
+   
+    });
+     //using delete method to delete record
+    it('Delete an existing post', () => {
+        cy.request({
+            method: 'DELETE',
+            url: baseUrl + "/posts/1", 
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((Response)=>{
+            expect(Response.status).to.equal(200);
+          
+        })  
+    });
 });
